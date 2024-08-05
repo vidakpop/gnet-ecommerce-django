5aa4e5a6cea707d0abeb23129bc1923fbf57ed41
DAVIS <davis@kali>
2024-08-05 13:32:22 +0300

    fixed error

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-!5+sa68067c3s=0bj1yjfyox-0e2i8du6^43#pnyj3l!3c^)*w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gnet-ecommerce-django-production.up.railway.app','https://gnet-ecommerce-django-production.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1','gnet-ecommerce-django-production.up.railway.app','https://gnet-ecommerce-django-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS=['https://gnet-ecommerce-django-production.up.railway.app']
 
 
