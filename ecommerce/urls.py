3cab873b9c66880fe40d7893891697dd9a90b0bc
DAVIS <davis@kali>
2024-08-05 12:28:49 +0300

    module error

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('store.urls')),
-    path('cart/',include('cart.urls')),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('', include('store.urls')),
+    path('cart/', include('cart.urls')),
+    
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
